4ecce11c51c9fc7f8bd84cf93d63f8cc9787984d
rakibmahmud742682-creator <tariqulislam.rahad123@gmail.com>
2026-04-28 18:09:45 +1000

    Ibad

---
+++ LibraryManagementSystem2.0-ICT272/Models/Fine.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Fine
+    {
+        public int Id { get; set; }
+
+        public int BorrowRecordId { get; set; }
+        public BorrowRecord BorrowRecord { get; set; } = null!;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+
+        [StringLength(200)]
+        public string? Reason { get; set; }
+
+        [Display(Name = "Issued Date")]
+        public DateTime IssuedDate { get; set; } = DateTime.Now;
+
+        [Display(Name = "Is Paid")]
+        public bool IsPaid { get; set; } = false;
+
+        public DateTime? PaidDate { get; set; }
+    }
+}
