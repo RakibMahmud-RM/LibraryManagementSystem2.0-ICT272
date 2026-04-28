64573c6a5b6d17d08b4d93c69ef0d77deee116b9
rakibmahmud742682-creator <rakibmahmud742682@gmail.com>
2026-04-28 16:26:28 +1000

    Add project files.

---
+++ LibraryManagementSystem2.0-ICT272/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryManagementSystem2._0_ICT272.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
